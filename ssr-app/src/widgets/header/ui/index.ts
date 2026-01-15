0125467398d0c272ce7f70e706ca150c3a14499f
Siarhei Buiko <innxfect@gmail.com>
2026-01-15 14:38:03 +0300

    JST-2587 feat(widgets/header): add header

---
+++ ssr-app/src/widgets/header/ui/index.ts
@@ -0,0 +1,7 @@
+export { Header } from './header';
+export { MobileMenu } from './mobileMenu';
+export { NavActions } from './navActions';
+export { NavElement } from './navElement';
+export { NavLinks } from './navLinks';
+export { renderNavIcon } from './renderNavIcon';
+export { UserMenu } from './userMenu';
